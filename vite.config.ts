0e44a095ad69f6b04622fda0c66267d2d46f34fd
kirubakarane-r <7n9g4ndn4w@privaterelay.appleid.com>
2026-07-01 20:23:08 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/',
+    base: '/fit-studio/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
